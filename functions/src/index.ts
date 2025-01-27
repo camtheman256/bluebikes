db412e3f8509ae3e6d6e7260498f71055fd4c159
Cameron Kleiman <camtheman256@gmail.com>
2025-01-26 19:02:01 -0500

    bump versions to keep collecting data

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,8 @@
-import * as functions from "firebase-functions";
-import {storage, initializeApp} from "firebase-admin";
+import * as functions from "firebase-functions/v1";
+import * as admin from "firebase-admin";
 import axios from "axios";
 import * as fs from "fs";
 import * as os from "os";
-
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -13,17 +12,20 @@ import * as os from "os";
 //   response.send("Hello from Firebase!");
 // });
 
-initializeApp({
+admin.initializeApp({
   storageBucket: "bluebikes-bbcc4.appspot.com",
 });
-const bucket = storage().bucket();
+const bucket = admin.storage().bucket();
 
 export const fetchBluebikesData = functions.pubsub
-    .schedule("every 1 hours synchronized").onRun(async (context) => {
-      const response = await axios.get("https://gbfs.bluebikes.com/gbfs/en/station_status.json");
-      const filepath = os.tmpdir() + "/station_status.json";
-      fs.writeFileSync(filepath, JSON.stringify(response.data));
-      return bucket.upload(filepath, {
-        destination: `status-${Date.now()}.json`,
-      });
+  .schedule("every 1 hours synchronized")
+  .onRun(async () => {
+    const response = await axios.get(
+      "https://gbfs.bluebikes.com/gbfs/en/station_status.json"
+    );
+    const filepath = os.tmpdir() + "/station_status.json";
+    fs.writeFileSync(filepath, JSON.stringify(response.data));
+    return bucket.upload(filepath, {
+      destination: `status-${Date.now()}.json`,
     });
+  });
